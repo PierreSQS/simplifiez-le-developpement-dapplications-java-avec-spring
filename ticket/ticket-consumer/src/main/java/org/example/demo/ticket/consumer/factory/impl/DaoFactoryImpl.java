1ff5a7250a02e18a0224b62f862ebf147cb0fc8f
PierreSQS <nomo_messina@web.de>
2020-06-10 22:01:56 +0200

    Commit 26an-Ok: Passage de la config XML -> config annotation non Java
    Constat faux au commit25
    
    Erreur dûe à l'annotation manquante dans DaoFactoryImpl
    
    ticket-batch.Main OK!

--- ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/factory/impl/DaoFactoryImpl.java
+++ ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/factory/impl/DaoFactoryImpl.java
@@ -1,7 +1,15 @@
 package org.example.demo.ticket.consumer.factory.impl;
 
+import org.example.demo.ticket.consumer.dao.contrat.ProjetDao;
+import org.example.demo.ticket.consumer.dao.contrat.TicketDao;
 import org.example.demo.ticket.consumer.factory.contrat.DaoFactory;
+import org.springframework.stereotype.Component;
 
+@Component("daoFactory")
 public class DaoFactoryImpl implements DaoFactory {
+	
+	private ProjetDao vPDao;
+	
+	private TicketDao vTDao;
 
 }
