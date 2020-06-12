3f6492504aed698f7f30e5ba668a69e38a1c120c
PierreSQS <nomo_messina@web.de>
2020-06-12 21:55:22 +0200

    Commit 35anxml-Ok: Attention aux Classes "@configuration"!
    Celles-ci importent un fichier *.xml non existant!
    
    Ne pas oublier nous sommes en mode Scan Annotations à partir d'1 XML!
    
    donc les classes "@configuration" sont considérées
    
    (voir ticket-batch.Main du projet, et ticket-webapp.bootstrapContext.xml
    ds commit31)

--- ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/configuration/SpringConfiguration.java
+++ ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/configuration/SpringConfiguration.java
@@ -1,7 +1,6 @@
 package org.example.demo.ticket.batch.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 //@Configuration
