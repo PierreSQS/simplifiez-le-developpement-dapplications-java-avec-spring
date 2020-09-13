e6b5e07b24b14360399fb3a297d589efc6ca6478
PierreSQS <nomo_messina@web.de>
2020-09-13 13:47:13 +0200

    Commit 44annjava-NOK: Essai de réécriture du Test
    La Datasource pas créée..

--- ticket/ticket-webapp/src/test/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResourceTest.java
+++ ticket/ticket-webapp/src/test/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResourceTest.java
@@ -1,38 +1,32 @@
 package org.example.demo.ticket.webapp.rest.resource.projet;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.example.demo.ticket.webapp.configuration.SpringTestConfiguration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 
 
-@SpringJUnitWebConfig(classes = SpringTestConfiguration.class)
+@WebMvcTest(ProjetResource.class)
 public class ProjetResourceTest {
 	
+	@Autowired
 	private MockMvc mockMvc;
-
-	@BeforeEach
-	public void setup(WebApplicationContext wac) {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-//		this.mockMvc = MockMvcBuilders.standaloneSetup(new ProjetResource()).build();
-	}
 
 	@Test
 	@Disabled("Not Ready yet")
 	public void testgetProjets() throws Exception {
 		this.mockMvc.perform(get("/ticket-webapp/projets").accept(MediaType.APPLICATION_JSON))
 							.andExpect(status().isOk())
+							.andDo(print())
 							.andExpect(content().contentType("application/json"))
 							.andExpect(jsonPath("$.name").value("Lee"));
 	}
